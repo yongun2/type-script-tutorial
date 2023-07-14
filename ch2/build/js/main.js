46a9b206ecaa6877f37937e5d5f59f259c0fc277
yongun2 <djkye0001@naver.com>
2023-07-14 15:25:49 +0900

    ch2

---
+++ ch2/build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = false;
+album = 123;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let re = /\w+/g;
---
+++ ch2/src/main.ts
@@ -0,0 +1,17 @@
+let myName:string;
+let meaningOfLife: number;
+let isLoading:boolean;
+let album: string | number;
+
+myName = 'John';
+meaningOfLife = 42;
+isLoading = false;
+album = 123;
+
+const sum = (a:number, b:string) => {
+    return a + b;
+};
+
+let postId: string | number;
+
+let re = /\w+/g
